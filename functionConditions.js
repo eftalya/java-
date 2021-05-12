dbd265dbb5cc9b2baa207fbc9eb8b8bdc1e68dae
eftalya <60375316+eftalya@users.noreply.github.com>
2021-05-11 23:04:34 -0400

    Create functionConditions.js

---
+++ functionConditions.js
@@ -0,0 +1,16 @@
+function lottery(button){
+  if (button == 1) {
+    return "You lose, loser!";
+  }else if(button == 2){
+    return "You lose, loser!";
+  }else if(button == 3){
+    return "You lose, loser!";
+  }else if (button == 4){
+    return "Damn you got some money!";
+  }
+}
+
+// console.log(button(1));
+// console.log(button(2));
+// console.log(button(3));
+// console.log(button(4));
